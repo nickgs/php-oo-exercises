1290b89ca2ef115773d5b89f03358a4c79e3de21
Nick Selvaggio <nickgs1@gmail.com>
2018-01-29 22:16:02 -0500

    Added some comment adjustments

--- simple_procedural_code.php
+++ simple_procedural_code.php
@@ -11,7 +11,7 @@ $firstname = "Nick";
 $lastname = "Selvaggio";
 $friends = ["Mike", "John","Steve","Jim", "Bob"];
 
-// Step 2. Lets manipulate the our friends. Simply Sort them.
+// Step 2. Lets manipulate our array of friends. Simply Sort them.
 asort($friends);
 
 
@@ -22,4 +22,5 @@ echo('</br>Has the following friends:</br>');
 print_r($friends);
 
 
-// Step 4. How can we make this better? 
+// How can we make this better? 
+// As this program grows what problems we think will emerge?
